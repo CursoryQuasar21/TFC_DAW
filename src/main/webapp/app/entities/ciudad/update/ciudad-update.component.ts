719c7c9d5d2a41e6db294272647011a0a77730dc
AlvaroDeVega <alvariito001@gmail.com>
2022-05-02 19:19:22 +0200

    Version_1.39-AF, Front-End, Ciudades, Paises

--- src/main/webapp/app/entities/ciudad/update/ciudad-update.component.ts
+++ src/main/webapp/app/entities/ciudad/update/ciudad-update.component.ts
@@ -9,6 +9,8 @@ import { ICiudad, Ciudad } from '../ciudad.model';
 import { CiudadService } from '../service/ciudad.service';
 import { IPais } from 'app/entities/pais/pais.model';
 import { PaisService } from 'app/entities/pais/service/pais.service';
+import { IAeropuerto } from 'app/entities/aeropuerto/aeropuerto.model';
+import { AeropuertoService } from 'app/entities/aeropuerto/service/aeropuerto.service';
 
 @Component({
   selector: 'jhi-ciudad-update',
@@ -16,6 +18,33 @@ import { PaisService } from 'app/entities/pais/service/pais.service';
 })
 export class CiudadUpdateComponent implements OnInit {
   isSaving = false;
+
+  // ==================================================================================================================================
+  // SECCION SWITCH PARA MOSTRAR Y OCULTAR TABLAS DE PERSONAS
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Switch que se va a encargar de controlar si el cuerpo de la tabla pilotos se muestra o no
+  hiddenAeropuertos = true;
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // FIN SECCION
+  // ==================================================================================================================================
+
+  // ==================================================================================================================================
+  // SECCION AEROPUERTO
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Lista para filtrar el aeropuerto
+  aeropuertosSharedCollection: IAeropuerto[] = [];
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Lista para filtrar el aeropuerto seleccionado
+  listaAeropuertosSeleccionados: IAeropuerto[] = [];
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // FIN SECCION
+  // ==================================================================================================================================
 
   paisSharedCollection: IPais[] = [];
 
@@ -26,6 +55,7 @@ export class CiudadUpdateComponent implements OnInit {
   });
 
   constructor(
+    protected aeropuertoService: AeropuertoService,
     protected ciudadService: CiudadService,
     protected paisService: PaisService,
     protected activatedRoute: ActivatedRoute,
@@ -37,6 +67,13 @@ export class CiudadUpdateComponent implements OnInit {
       this.updateForm(ciudad);
 
       this.loadRelationshipsOptions();
+
+      // -------------------------------------------------------------------------------------------------------------------------------
+
+      // Metodo que cargan las listas de las zonas
+      this.loadListasZonas(ciudad);
+
+      // -------------------------------------------------------------------------------------------------------------------------------
     });
   }
 
@@ -54,9 +91,146 @@ export class CiudadUpdateComponent implements OnInit {
     }
   }
 
+  // ==================================================================================================================================
+  // SECCION DE METODOS PARA MANEJAR LAS LISTAS EN EL HTML
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Metodo Agregado
+  trackAeropuertoById(index: number, item: IAeropuerto): number {
+    return item.id!;
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Metodo Agregado
   trackPaisById(index: number, item: IPais): number {
     return item.id!;
   }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // FIN SECCION
+  // ==================================================================================================================================
+
+  // ==================================================================================================================================
+  // SECCION MOSTRAR OCULTAR TABLAS DE PERSONAS
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Metodo agregado
+  // Metodo encargado de mostrar y ocultar la tabla indicada por parametro
+  public mostrarOcultarTabla(i: number, e: any): void {
+    const elemento = document.getElementsByClassName('custom-control-label')[i];
+    if (elemento.innerHTML === 'Ocultar') {
+      elemento.innerHTML = 'Mostrar';
+    } else {
+      elemento.innerHTML = 'Ocultar';
+    }
+    switch (e) {
+      case 'cuerpoTablaAeropuertos': {
+        this.hiddenAeropuertos = !this.hiddenAeropuertos;
+        break;
+      }
+    }
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // FIN SECCION
+  // ==================================================================================================================================
+
+  // ==================================================================================================================================
+  // SECCION AEROPUERTOS
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Metodo Agregado
+  // Cargar Listas de aeropuertos
+  public loadListasZonas(ciudad: ICiudad): void {
+    this.loadAeropuertos(ciudad);
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Metodo Agregado
+  // Cargar aeropuertos
+  public loadAeropuertos(ciudad: ICiudad): void {
+    this.aeropuertosSharedCollection = [];
+    this.aeropuertoService.query().subscribe((res: HttpResponse<IAeropuerto[]>) => {
+      res.body?.forEach((aeropuerto: IAeropuerto) => {
+        // Condicion que determina si el aeropuerto esta asignado a una ciudad
+        if (aeropuerto.ciudad === null) {
+          // El aeropuerto no esta asigando a una ciudad y por tanto se puede asignar a una ciudad
+          this.aeropuertosSharedCollection.push(aeropuerto);
+        } else {
+          // Condicion que determina si el aeropuerto que tiene una ciudad asignada es la misma ciudad que la que se esta mostrando
+          if (ciudad.id === aeropuerto.ciudad?.id) {
+            this.aeropuertosSharedCollection.push(aeropuerto);
+            // Refleja que el aeropuerto ya esta añadido a la lista de aeropuerto ya que se añadio anteriormente en una modificacion anterior de la ciudad
+            this.anadirAeropuertoSeleccionado(aeropuerto);
+          }
+        }
+      });
+    });
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Metodo Agregado
+  // Metodo que añade un aeropuerto a la lista de aeropuertos
+  public anadirAeropuertoSeleccionado(aeropuerto: IAeropuerto): void {
+    this.listaAeropuertosSeleccionados.push(aeropuerto);
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // FIN SECCION
+  // ==================================================================================================================================
+
+  // ==================================================================================================================================
+  // SECCION PARA ACTUALIZAR LISTAS AEROPUERTOS Y ACTUALIZAR COMPONENTES DEL HTML
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Metodo Agregado
+  // Metodo para añadir aeropuertos a la lista de aeropuertos seleccionados y actualizar componentes del html de la tabla de aeropuertos
+  public anadirAeropuerto(aeropuerto: IAeropuerto, e: any): void {
+    // Camnbia el tipo de boton segun si el boton esta en añadir o eliminar
+    if (e.classList.contains('btn-outline-primary')) {
+      // Añade el aeropuerto a la lista de aeropuerto seleccionados
+      this.listaAeropuertosSeleccionados.push(aeropuerto);
+      // Reordenacion de la lista de pilotos abordo
+      this.listaAeropuertosSeleccionados = this.ordenarListaAeropuertosSeleccionada(this.listaAeropuertosSeleccionados);
+      // Cambia las propiedades del boton
+      e.classList.remove('btn-outline-primary');
+      e.classList.add('btn-outline-danger');
+      e.textContent = 'Eliminar';
+    } else {
+      //  Recorre la lista identificando, para eliminar el piloto de la lista de pilotos abordo
+      for (let i = 0; i < this.listaAeropuertosSeleccionados.length; i++) {
+        if (this.listaAeropuertosSeleccionados[i] === aeropuerto) {
+          this.listaAeropuertosSeleccionados.splice(i, 1);
+        }
+      }
+      // Cambia las propiedades del boton
+      e.classList.remove('btn-outline-danger');
+      e.classList.add('btn-outline-primary');
+      e.textContent = 'Añadir';
+    }
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  //  Metodo agregado
+  //  Devuelve una lista ordenada de la lista de aeropuertos seleccionada
+  public ordenarListaAeropuertosSeleccionada(aeropuertos: IAeropuerto[]): IAeropuerto[] {
+    aeropuertos.sort(function (a, b) {
+      if (a.id && b.id) {
+        return a.id - b.id;
+      } else {
+        return 0;
+      }
+    });
+    return aeropuertos;
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // FIN SECCION
+  // ==================================================================================================================================
 
   protected subscribeToSaveResponse(result: Observable<HttpResponse<ICiudad>>): void {
     result.pipe(finalize(() => this.onSaveFinalize())).subscribe(
--- src/main/webapp/app/entities/pais/update/pais-update.component.ts
+++ src/main/webapp/app/entities/pais/update/pais-update.component.ts
@@ -7,6 +7,8 @@ import { finalize } from 'rxjs/operators';
 
 import { IPais, Pais } from '../pais.model';
 import { PaisService } from '../service/pais.service';
+import { ICiudad } from 'app/entities/ciudad/ciudad.model';
+import { CiudadService } from 'app/entities/ciudad/service/ciudad.service';
 
 @Component({
   selector: 'jhi-pais-update',
@@ -15,16 +17,55 @@ import { PaisService } from '../service/pais.service';
 export class PaisUpdateComponent implements OnInit {
   isSaving = false;
 
+  // ==================================================================================================================================
+  // SECCION SWITCH PARA MOSTRAR Y OCULTAR TABLAS DE PERSONAS
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Switch que se va a encargar de controlar si el cuerpo de la tabla pilotos se muestra o no
+  hiddenCiudades = true;
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // FIN SECCION
+  // ==================================================================================================================================
+
+  // ==================================================================================================================================
+  // SECCION CIUDADES
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Lista para filtrar la ciudad
+  ciudadesSharedCollection: ICiudad[] = [];
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Lista para filtrar la ciudad seleccionado
+  listaCiudadesSeleccionadas: ICiudad[] = [];
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // FIN SECCION
+  // ==================================================================================================================================
+
   editForm = this.fb.group({
     id: [],
     nombre: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
   });
 
-  constructor(protected paisService: PaisService, protected activatedRoute: ActivatedRoute, protected fb: FormBuilder) {}
+  constructor(
+    protected paisService: PaisService,
+    protected ciudadService: CiudadService,
+    protected activatedRoute: ActivatedRoute,
+    protected fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ pais }) => {
       this.updateForm(pais);
+
+      // -------------------------------------------------------------------------------------------------------------------------------
+
+      // Metodo que cargan las listas de las zonas
+      this.loadListasZonas(pais);
+
+      // -------------------------------------------------------------------------------------------------------------------------------
     });
   }
 
@@ -41,6 +82,140 @@ export class PaisUpdateComponent implements OnInit {
       this.subscribeToSaveResponse(this.paisService.create(pais));
     }
   }
+
+  // ==================================================================================================================================
+  // SECCION DE METODOS PARA MANEJAR LAS LISTAS EN EL HTML
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Metodo Agregado
+  trackCiudadById(index: number, item: ICiudad): number {
+    return item.id!;
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // FIN SECCION
+  // ==================================================================================================================================
+
+  // ==================================================================================================================================
+  // SECCION MOSTRAR OCULTAR TABLAS DE PERSONAS
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Metodo agregado
+  // Metodo encargado de mostrar y ocultar la tabla indicada por parametro
+  public mostrarOcultarTabla(i: number, e: any): void {
+    const elemento = document.getElementsByClassName('custom-control-label')[i];
+    if (elemento.innerHTML === 'Ocultar') {
+      elemento.innerHTML = 'Mostrar';
+    } else {
+      elemento.innerHTML = 'Ocultar';
+    }
+    switch (e) {
+      case 'cuerpoTablaCiudades': {
+        this.hiddenCiudades = !this.hiddenCiudades;
+        break;
+      }
+    }
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // FIN SECCION
+  // ==================================================================================================================================
+
+  // ==================================================================================================================================
+  // SECCION CIUDADES
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Metodo Agregado
+  // Cargar Listas de aeropuertos
+  public loadListasZonas(pais: IPais): void {
+    this.loadCiudades(pais);
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Metodo Agregado
+  // Cargar ciudades
+  public loadCiudades(pais: IPais): void {
+    this.ciudadesSharedCollection = [];
+    this.ciudadService.query().subscribe((res: HttpResponse<ICiudad[]>) => {
+      res.body?.forEach((ciudad: ICiudad) => {
+        // Condicion que determina si la ciudad esta asignado a un pais
+        if (ciudad.pais === null) {
+          // La ciudad no esta asigando a un pais y por tanto se puede asignar a un pais
+          this.ciudadesSharedCollection.push(ciudad);
+        } else {
+          // Condicion que determina si la ciudad que tiene un pais asignado es el mismo pais que el que se esta mostrando
+          if (pais.id === ciudad.pais?.id) {
+            this.ciudadesSharedCollection.push(ciudad);
+            // Refleja que la ciudad ya esta añadido a la lista de ciudades  ya que se añadio anteriormente en una modificacion anterior del pais
+            this.anadirCiudadSeleccionado(ciudad);
+          }
+        }
+      });
+    });
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Metodo Agregado
+  // Metodo que añade una ciudad a la lista de ciudades
+  public anadirCiudadSeleccionado(ciudad: ICiudad): void {
+    this.listaCiudadesSeleccionadas.push(ciudad);
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // FIN SECCION
+  // ==================================================================================================================================
+
+  // ==================================================================================================================================
+  // SECCION PARA ACTUALIZAR LISTAS AEROPUERTOS Y ACTUALIZAR COMPONENTES DEL HTML
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // Metodo Agregado
+  // Metodo para añadir ciudades a la lista de ciudades seleccionadas y actualizar componentes del html de la tabla de ciudades
+  public anadirCiudad(ciudad: ICiudad, e: any): void {
+    // Camnbia el tipo de boton segun si el boton esta en añadir o eliminar
+    if (e.classList.contains('btn-outline-primary')) {
+      // Añade el aeropuerto a la lista de aeropuerto seleccionados
+      this.listaCiudadesSeleccionadas.push(ciudad);
+      // Reordenacion de la lista de pilotos abordo
+      this.listaCiudadesSeleccionadas = this.ordenarListaCiudadesSeleccionada(this.listaCiudadesSeleccionadas);
+      // Cambia las propiedades del boton
+      e.classList.remove('btn-outline-primary');
+      e.classList.add('btn-outline-danger');
+      e.textContent = 'Eliminar';
+    } else {
+      //  Recorre la lista identificando, para eliminar el piloto de la lista de pilotos abordo
+      for (let i = 0; i < this.listaCiudadesSeleccionadas.length; i++) {
+        if (this.listaCiudadesSeleccionadas[i] === ciudad) {
+          this.listaCiudadesSeleccionadas.splice(i, 1);
+        }
+      }
+      // Cambia las propiedades del boton
+      e.classList.remove('btn-outline-danger');
+      e.classList.add('btn-outline-primary');
+      e.textContent = 'Añadir';
+    }
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // -------------------------------------------------------------------------------------------------------------------------------
+  //  Metodo agregado
+  //  Devuelve una lista ordenada de la lista de ciudades seleccionadas
+  public ordenarListaCiudadesSeleccionada(ciudades: ICiudad[]): ICiudad[] {
+    ciudades.sort(function (a, b) {
+      if (a.id && b.id) {
+        return a.id - b.id;
+      } else {
+        return 0;
+      }
+    });
+    return ciudades;
+  }
+  // -------------------------------------------------------------------------------------------------------------------------------
+
+  // FIN SECCION
+  // ==================================================================================================================================
 
   protected subscribeToSaveResponse(result: Observable<HttpResponse<IPais>>): void {
     result.pipe(finalize(() => this.onSaveFinalize())).subscribe(
